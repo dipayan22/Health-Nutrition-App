494142ed71b07aebfa1626a958e89cba11db85e5
Dipayan Majumder <110282003+dipayan22@users.noreply.github.com>
2024-02-01 17:41:48 +0530

    App Updated

--- app.py
+++ app.py
@@ -31,38 +31,40 @@ def input_image_setup(uploader_file):
     else:
         raise FileNotFoundError('No file Uploaded')
 
+def main():
+    st.set_page_config(page_title='Calories Advissor APP')
 
-st.set_page_config(page_title='Calories Advissor APP')
+    st.header('Gemini Health App')
+    uploader_file=st.file_uploader('Choose an image ...',type=['jpg','png','jpeg'])
+    image=""
 
-st.header('Gemini Health App')
-uploader_file=st.file_uploader('Choose an image ...',type=['jpg','png','jpeg'])
-image=""
-
-if uploader_file is not None:
-    image=Image.open(uploader_file)
-    st.image(image,caption='Uploaded Image',use_column_width=True)
-
-
-submit=st.button("Tell me About the toatl calories")
-
-input_prompt='''
-    you are an expert in nutritionist where you need to see the food items from the image and calculate the total calories , also provide the details of every food items with calories intakes in the below format 
-
-    1. item 1 - no of calories
-    2. item 2 - no of calories
-    ----
-    ----
-
-    Finally ypu can also mention wheather the food is healthy or not and also mention the percentage split of the ratio of carbohydrates,fats,fibers,suger and pther important things required in our diet
-    
-'''
-# I am feeling hungry and I want to know how much food should I eat today
+    if uploader_file is not None:
+        image=Image.open(uploader_file)
+        st.image(image,caption='Uploaded Image',use_column_width=True)
 
 
-if submit:
-    image_data=input_image_setup(uploader_file)
-    response=get_gemini_response(input_prompt,image_data)
+    submit=st.button("Tell me About the toatl calories")
 
-    st.header("the response is :")
-    st.write(response)
+    input_prompt='''
+        you are an expert in nutritionist where you need to see the food items from the image and calculate the total calories , also provide the details of every food items with calories intakes in the below format 
 
+        1. item 1 - no of calories
+        2. item 2 - no of calories
+        ----
+        ----
+
+        Finally ypu can also mention wheather the food is healthy or not and also mention the percentage split of the ratio of carbohydrates,fats,fibers,suger and pther important things required in our diet
+        
+    '''
+    # I am feeling hungry and I want to know how much food should I eat today
+
+
+    if submit:
+        image_data=input_image_setup(uploader_file)
+        response=get_gemini_response(input_prompt,image_data)
+
+        st.header("the response is :")
+        st.write(response)
+
+if __name__ == "__main__":
+    main()
